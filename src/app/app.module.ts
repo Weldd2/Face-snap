6113ee85022cfc4ff1a02866bd2e898c73854e26
LAPTOP-4EF00T9E\antoi <22003037.etu.unicaen.fr>
2022-01-22 16:03:36 +0100

    Création d'un composant face-snap vide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FaceSnapComponent} from './face-snap/face-snap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceSnapComponent
   ],
   imports: [
     BrowserModule
