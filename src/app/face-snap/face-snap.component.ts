7bc0ba2ad6d4c8725d5103f4b7cf93cb97226bf2
LAPTOP-4EF00T9E\antoi <22003037.etu.unicaen.fr>
2022-01-22 16:27:54 +0100

    Modification du composant et modifications du style

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,9 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: String;
+
+  ngOnInit() {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis tout petit !';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://profile-images.xing.com/images/73c18beb6d87c6bcc208b85465857015-1/antoine-mura.256x256.jpg'
+  }
 }
